ab1846e2bbd3e0d2fd8877040760dbfb1abd4555
Abhishek-s-kumar <145575718+Abhishek-s-kumar@users.noreply.github.com>
2026-06-27 13:35:28 +0530

    Add files via upload

---
+++ ambw.py
@@ -0,0 +1,36 @@
+import asyncio
+import mss
+import numpy as np
+from bleak import BleakClient
+
+ADDR = "58:01:EA:A7:17:2D"
+WRITE_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
+
+EDGE_PX = 60
+WEIGHTS = [0.25, 0.25, 0.25, 0.25]  # [top, bottom, left, right]
+
+def sample_edges_weighted(img):
+    h, w, _ = img.shape
+    e = min(EDGE_PX, h // 4, w // 4)
+    top_avg = img[0:e, :, :3].reshape(-1, 3).mean(axis=0)
+    bottom_avg = img[h - e:h, :, :3].reshape(-1, 3).mean(axis=0)
+    left_avg = img[:, 0:e, :3].reshape(-1, 3).mean(axis=0)
+    right_avg = img[:, w - e:w, :3].reshape(-1, 3).mean(axis=0)
+    stacked = np.stack([top_avg, bottom_avg, left_avg, right_avg])
+    weighted = np.average(stacked, axis=0, weights=WEIGHTS)
+    b, g, r = weighted.astype(int)
+    return r, g, b
+
+async def ambilight():
+    async with BleakClient(ADDR) as client:
+        await client.write_gatt_char(WRITE_UUID, bytearray([0xA0, 0x62, 0x01, 0x01]), True)
+        with mss.mss() as sct:
+            monitor = sct.monitors[1]
+            while True:
+                img = np.array(sct.grab(monitor))
+                r, g, b = sample_edges_weighted(img)
+                cmd = bytearray([0xA0, 0x69, 0x04, r, g, b, 255])
+                await client.write_gatt_char(WRITE_UUID, cmd, False)
+                await asyncio.sleep(0.15)
+
+asyncio.run(ambilight())
